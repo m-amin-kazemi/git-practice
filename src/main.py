46b865cbab6faa5e9bf372f816fa9d4d3a05326b
Mohamadamin <mohamadaminkazemi1997@gmail.com>
2026-05-14 08:31:45 +0330

    Add a message for rebasing

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ print("This program demonstrates how to use a function from another module.")
 print("Thank you for using this program!")
 print("Goodbye!")
 print("This is the end of the program.")
+print("This is a message to learn how rebasing works in Git.")
+
 print("This program is now complete.")
 print("this message is how git rebasing works")
 
